79afc28ef07418ee42ddda410f26569f394629e4
Andrew Buchan <andyhasit@gmail.com>
2026-01-17 17:52:57 +0000

    updated babel config files

--- templates/base/README.md
+++ templates/base/README.md
@@ -6,4 +6,4 @@ Your project should be installed and ready to run with:
 npm start
 ```
 
-For documentation visit: https://github.com/wallace-js/wallace
+For documentation visit: https://wallace.js.org/docs/
--- templates/typescript/babel.config.cjs
+++ templates/typescript/babel.config.cjs
@@ -1,4 +1,16 @@
 module.exports = {
-  plugins: ["babel-plugin-wallace", "@babel/plugin-syntax-jsx"],
+  plugins: [
+    [
+      "babel-plugin-wallace",
+      {
+        flags: {
+          useControllers: true,
+          useMethods: true,
+          useStubs: true,
+        },
+      },
+    ],
+    "@babel/plugin-syntax-jsx",
+  ],
   presets: ["@babel/preset-typescript"],
 };
